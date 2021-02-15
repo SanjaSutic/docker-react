fb7a818c2559eea7d86bcca71ae25d84b129d1d2
Sanja Sutic <sanja.sutic@investec.co.za>
2021-02-15 22:40:22 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Thanks Rich love you lots and lots and some more lots!
         </p>
         <a
           className="App-link"
